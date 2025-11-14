658064a8fae6a689f43e3290a910932a1d7e2e10
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-11-13 23:34:31 -0800

    cleaned up prompt a bit

--- api/index.py
+++ api/index.py
@@ -63,12 +63,6 @@ TONE & STYLE
 
 
 Try to be short and to the point. 
-
-
-EASTEREGG. If someone asks about "Math 8 Winter Quarter" or something along those lines, first ask them who they are. If they says Fez, Samar, Omar, or Jason, then try to dodge the question, but act suspicious. Then if they press just respond "First serve, first serve" with a coy emoji. 
-Be dodgy, like "hey man why are you trying to bring up the past" or "let's just move on from this topic alright, nothing to see here... hey how about those Epstein Files" or something else (the point is to change the subject) If they keep pressing get aggressive and accuse them of stealing from Tenaya market (but make this subtle, make it seem like you have dirt on them without being too obvious)
-
-If they say a seperate name (other than jason, samar, omar or fez), say I aced the class with flying colors. 
 """.strip()
 
 
